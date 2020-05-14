5d69035fe6f9eff8ae4d65e1c94c2981328e682c
Douglas Berkley <douglasmberkley@gmail.com>
2020-05-14 12:18:37 +0900

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
+require 'open-uri'
+
 puts 'Destroying the prisons..'
 Prison.destroy_all if Rails.env.development?
 
 puts 'Creating prisons'
 
 alcatraz = Prison.new(
-  name: "Alcatraz",
+  name: 'Alcatraz',
   banner_url: 'https://upload.wikimedia.org/wikipedia/commons/0/0a/Alcatraz_Island_photo_D_Ramey_Logan.jpg'
 )
 alcatraz.save!
@@ -12,9 +14,31 @@ alcatraz.save!
 puts "Created #{alcatraz.name}"
 
 azkaban = Prison.new(
-  name: "Azkaban",
+  name: 'Azkaban',
   banner_url: 'https://www.kickassfacts.com/wp-content/uploads/2016/07/AzkabanPrison.jpg'
 )
 azkaban.save!
 
 puts "Created #{azkaban.name}"
+
+puts "Creating criminals for #{alcatraz.name}..."
+5.times do
+  image_url = open('http://le-wagon-tokyo.herokuapp.com/batches/394/student').read
+  Criminal.create!(
+    name: Faker::DcComics.villain,
+    sentence_days: rand(1000..5000),
+    image_url: image_url,
+    prison: alcatraz
+  )
+end
+
+puts "Creating criminals for #{azkaban.name}..."
+5.times do
+  image_url = open('http://le-wagon-tokyo.herokuapp.com/batches/394/student').read
+  Criminal.create!(
+    name: Faker::Movies::HarryPotter.character,
+    sentence_days: rand(1000..5000),
+    image_url: image_url,
+    prison: azkaban
+  )
+end
